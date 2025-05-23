7e98c6b01b4f506ff1fe2ba55750cffa51e165dd
Bogdanov K.A. <bogdanokir21897@gmail.com>
2025-05-23 18:42:50 +0300

    Add advanced ex.

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import streamlit as st
 # Навигация между страницами
 pages = {
     "Анализ данных и модель": [st.Page("analysis_and_model.py", title="Анализ данных и модель")],
-    "Презентация проекта": [st.Page(" presentation.py", title="Презентация проекта")]
+    "Презентация проекта": [st.Page(" presentation.py", title="Презентация проекта")],
+    "EDA": [st.Page("eda_page.py", title="Исследовательский анализ данных")]
 }
 
 # Отображение меню навигации
